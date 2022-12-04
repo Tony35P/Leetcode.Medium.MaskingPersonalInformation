6e5912fa86f00a68d1b729299b1cf29bf5304b08
Tony <tonytsai27@gmail.com>
2022-12-04 19:18:23 +0800

    完成 MaskingPersonalInformation, 並將MaskEmailAdress, MaskPhoneNumber抽離出來, 讓MaskPII功能更容易理解

--- Leetcode.Medium.MaskingPersonalInformation/MaskingHelper.cs
+++ Leetcode.Medium.MaskingPersonalInformation/MaskingHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Leetcode.Medium.MaskingPersonalInformation
@@ -13,15 +14,37 @@ namespace Leetcode.Medium.MaskingPersonalInformation
             int indexOfAt = data.IndexOf("@");
             if (indexOfAt > 0)
             {
-                string lowerEmail = data.ToLower();
-
-                string result = $"{lowerEmail.Substring(0, 1)}*****{lowerEmail.Substring(indexOfAt - 1)}";
-                return result;
+                return MaskEmailAdress(data);
             }
             else
             {
-                throw new NotImplementedException();
+                return MaskPhoneNumber(data);
             }
+        }
+
+        public string MaskPhoneNumber(string data)
+        {
+            string regexPhoneNumber = Regex.Replace(data, "[^0-9]", string.Empty);
+            int phoneNumberLength = regexPhoneNumber.Length;
+            string last4Yards = regexPhoneNumber.Substring(phoneNumberLength - 4);
+
+            switch (phoneNumberLength)
+            {
+                case 10: return "***-***-" + last4Yards;
+                case 11: return "+*-***-***-" + last4Yards;
+                case 12: return "+**-***-***-" + last4Yards;
+            }
+
+            return "+***-***-***-" + last4Yards;
+        }
+
+        public string MaskEmailAdress(string data)
+        {
+            int indexOfAt = data.IndexOf("@");
+            string lowerEmail = data.ToLower();
+
+            string result = $"{lowerEmail.Substring(0, 1)}*****{lowerEmail.Substring(indexOfAt - 1)}";
+            return result;
         }
     }
 }
